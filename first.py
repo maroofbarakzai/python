14c08c10b1f929afc88fb165c061d15e8cbb8df8
maroofbarakzai <maroof.barakzai12@gmail.com>
2025-02-11 13:54:41 +0430

    [feat]?

--- first.py
+++
@@ -1,4 +0,0 @@
-print("welcome to python")
-a = 10
-b= 20
-print(a+b)
---
+++ second.py
@@ -0,0 +1,34 @@
+
+# # a=15
+# # b=a+5
+# # print ("the some of a and b is ",a+b) 
+# # r=5
+# # pie =3.14
+# # print("the area is ",pie*r*r)
+# import os
+# import msvcrt
+# a=10
+# b=a+5
+# os.system('cls' if os.name == 'nt' else 'clear')
+# # Print values of 'a' and 'b'
+# print(f"A is {a} and B is {b}")
+# print("Press any key to finish")
+# # Wait for a key press
+# msvcrt.getch()
+a=0
+m=0
+z=4
+a=z*2 #8
+m=z*z #16 
+print("m=",m) # 16
+print("a=",a) # 8
+#half b*h
+a = 12
+b = 5
+m = 0.5*a*b
+print("m=",m)
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+
